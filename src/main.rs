8b9565f98c7c396fa332915f23a10ec6047697e5
Aliaume LOPEZ <ad.lopez@uw.edu.pl>
2024-10-11 14:28:08 +0200

    Better command line arguments

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,23 @@ use bibadac::linter::{LintMessage, LinterState};
 
 use std::collections::HashSet;
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
+
+fn windowed(s: &str, start: usize, end: usize, window_size: usize) -> (&str, &str, &str) {
+    let new_start_attempt = start.saturating_sub(window_size);
+    let new_end_attempt = end + window_size;
+    let new_start = s
+        .char_indices()
+        .nth(new_start_attempt)
+        .map(|(i, _)| i)
+        .unwrap_or(0);
+    let new_end = s
+        .char_indices()
+        .nth(new_end_attempt)
+        .map(|(i, _)| i)
+        .unwrap_or(s.len());
+    (&s[new_start..start], &s[start..end], &s[end..new_end])
+}
 
 #[derive(Debug, Clone, Parser)]
 #[command(name = "bibadac")]
@@ -32,7 +48,10 @@ struct Cli {
 
 #[derive(Debug, Clone, Subcommand)]
 enum SubCommand {
-    #[command(about = "Check the validity of a BibTeX/BibLaTeX file", arg_required_else_help = true)]
+    #[command(
+        about = "Check the validity of a BibTeX/BibLaTeX file",
+        arg_required_else_help = true
+    )]
     Check(CheckArgs),
     #[command(about = "Format a BibTeX/BibLaTeX file", arg_required_else_help = true)]
     Format(FormatArgs),
@@ -42,35 +61,52 @@ enum SubCommand {
 
 #[derive(Debug, Clone, Args)]
 struct FileArgs {
-    #[arg(short, long, help="BibTeX/BibLaTeX files to read")]
-    bib: Vec<std::path::PathBuf>,
-    #[arg(short, long, help="Read BibTeX from stdin")]
+    #[arg(
+        short,
+        long,
+        help = "Read BibTeX from stdin, set to true in case no bibfiles are provided"
+    )]
     stdin: bool,
+    /// BibTeX/BibLaTeX files to read
+    bib: Vec<std::path::PathBuf>,
 }
 
 #[derive(Debug, Clone, Args)]
 struct CheckArgs {
     #[clap(flatten)]
     files: FileArgs,
-    #[arg(short, long, help = "Be pedantic and show all errors")]
-    pedantic: bool,
-    #[arg(short, long, help = "Show location of the errors")]
-    verbose: bool,
+    #[arg(short, long, help = "Show only important errors")]
+    concise: bool,
+    #[arg(short, long, help = "Hide location of errors to symplify output")]
+    executive_summary: bool,
     #[arg(short, long, help = "Output the errors in JSON format")]
     to_json: bool,
 }
 
-
 #[derive(Debug, Clone, Args)]
 struct FormatArgs {
     #[clap(flatten)]
-    files: FileArgs, 
+    files: FileArgs,
     #[arg(short, long, help = "Create a new file with the formatted content")]
     to_file: bool,
     #[arg(short, long, help = "Update the files *in place* (dangerous)")]
     in_place: bool,
     #[arg(short, long, help = "Autocomplete entries using an existing bibfile")]
     file_db: Option<std::path::PathBuf>,
+    #[arg(short, long, help = "Remove the corresponding fields from the output")]
+    remove_field: Vec<String>,
+    #[arg(short, long, help = "Only keep the corresponding fields in the output")]
+    keep_field: Vec<String>,
+    #[arg(
+        short,
+        long,
+        help = "Only keep entries containing one of the following fields"
+    )]
+    entry_field: Vec<String>,
+    #[arg(short = 'l', long, help = "Order the fields alphabetically")]
+    sort_fields: bool,
+    #[arg(short = 'g', long, help = "Order the entries alphabetically")]
+    sort_entries: bool,
 }
 
 #[derive(Debug, Clone, Args)]
@@ -101,6 +137,7 @@ trait InputFiles {
 
 impl InputFiles for FileArgs {
     fn list_files(&self) -> Vec<InputFile> {
+        let use_stdin = self.stdin || self.bib.is_empty();
         self.bib
             .iter()
             .filter_map(|name| {
@@ -113,7 +150,8 @@ impl InputFiles for FileArgs {
                     name: name.clone(),
                     content,
                 })
-            }).chain(if self.stdin {
+            })
+            .chain(if use_stdin {
                 let mut content = String::new();
                 std::io::stdin().read_to_string(&mut content).unwrap();
                 vec![InputFile {
@@ -122,7 +160,8 @@ impl InputFiles for FileArgs {
                 }]
             } else {
                 vec![]
-            }).collect()
+            })
+            .collect()
     }
 }
 
@@ -158,43 +197,58 @@ fn main() {
                 arxiv_doi: Default::default(),
             };
             let files = cargs.files.list_files();
-            let inputs = files.iter().map(|f| {
-                let bibtex = BibFile::new(&f.content);
-                (f, bibtex)
-            }).collect::<Vec<_>>();
+            let inputs = files
+                .iter()
+                .map(|f| {
+                    let bibtex = BibFile::new(&f.content);
+                    (f, bibtex)
+                })
+                .collect::<Vec<_>>();
             let mut lints = vec![];
             for (bib, tex) in inputs.iter() {
-                if cargs.pedantic {
-                    lints.push((bib, tex, linter.lint_file(&tex, tex.list_entries().collect())));
+                if !cargs.concise {
+                    lints.push((
+                        bib,
+                        tex,
+                        linter.lint_file(&tex, tex.list_entries().collect()),
+                    ));
                 } else {
-                    lints.push((bib, tex, 
-                                linter.lint_file(&tex, tex.list_entries().collect())
-                                      .into_iter()
-                                      .filter(|l| l.msg.is_crucial()).collect()));
+                    lints.push((
+                        bib,
+                        tex,
+                        linter
+                            .lint_file(&tex, tex.list_entries().collect())
+                            .into_iter()
+                            .filter(|l| l.msg.is_crucial())
+                            .collect(),
+                    ));
                 }
             }
 
             if cargs.to_json {
                 let mut out = std::io::stdout();
-                let json_report = lints.iter()
-                    .map(|(bib, _, lints)| {
-                        JsonReportEntry {
-                            file: bib.name.to_string_lossy().to_string(),
-                            errors: lints.iter().map(|l| {
-                                JsonReportLint {
-                                    msg: l.msg.clone(),
-                                    loc: l.loc.iter().map(|n| {
-                                        JsonReportLoc {
-                                            line: n.start_position().row + 1,
-                                            column: n.start_position().column + 1,
-                                            start_byte: n.start_byte(),
-                                            end_byte: n.end_byte(),
-                                        }
-                                    }).collect()
-                                }
-                            }).collect()
-                        }
-                    }).collect::<Vec<_>>();
+                let json_report = lints
+                    .iter()
+                    .map(|(bib, _, lints)| JsonReportEntry {
+                        file: bib.name.to_string_lossy().to_string(),
+                        errors: lints
+                            .iter()
+                            .map(|l| JsonReportLint {
+                                msg: l.msg.clone(),
+                                loc: l
+                                    .loc
+                                    .iter()
+                                    .map(|n| JsonReportLoc {
+                                        line: n.start_position().row + 1,
+                                        column: n.start_position().column + 1,
+                                        start_byte: n.start_byte(),
+                                        end_byte: n.end_byte(),
+                                    })
+                                    .collect(),
+                            })
+                            .collect(),
+                    })
+                    .collect::<Vec<_>>();
                 serde_json::to_writer_pretty(&mut out, &json_report).unwrap();
                 return;
             }
@@ -202,52 +256,55 @@ fn main() {
             // 1. print the number of errors for every input
             for (bib, _, lints) in lints.iter() {
                 if lints.len() == 0 {
-                    println!("{} {:?}", "[OK]".green(), bib.name);
+                    println!("{} \t\t {:?}", "[OK]".green(), bib.name);
                 } else {
                     let err = if lints.len() > 1 { "errors" } else { "error" };
-                    println!("{} {:?} \t {} {}", 
-                             "[KO]".red(),
-                             bib.name,
-                             lints.len(),
-                             err);
+                    println!("{} {} {} \t {:?}", "[KO]".red(), lints.len(), err, bib.name);
                 }
             }
             // 2. print the errors for each file if verbose
-            if !cargs.verbose {
+            if cargs.executive_summary {
                 return;
             }
 
             for (bib, bibtex, lints) in lints.iter() {
                 for l in lints {
-                    println!("{}\n<{:?}:L{}:C{}>\n{:?}", 
-                             "Error".red(),
-                             bib.name, 
-                             l.loc[0].start_position().row    + 1,
-                             l.loc[0].start_position().column + 1,
-                             l.msg);
-                    println!("{}",
-                                l.loc.iter()
-                                    .map(|n| {
-                                        let s = bibtex.get_slice(*n);
-                                        s.lines()
-                                         .take(3)
-                                         .zip(1..)
-                                         .map(|(l, i)| format!("{:>4}| {}", i + n.start_position().row, l))
-                                         .collect::<Vec<_>>()
-                                         .join("\n")
+                    println!(
+                        "{}\n<{:?}:L{}:C{}>\n{:?}",
+                        "Error".red(),
+                        bib.name,
+                        l.loc[0].start_position().row + 1,
+                        l.loc[0].start_position().column + 1,
+                        l.msg
+                    );
+                    println!(
+                        "{}",
+                        l.loc
+                            .iter()
+                            .map(|n| {
+                                let s = bibtex.get_slice(*n);
+                                s.lines()
+                                    .take(3)
+                                    .zip(1..)
+                                    .map(|(l, i)| {
+                                        format!("{:>4}| {}", i + n.start_position().row, l)
                                     })
                                     .collect::<Vec<_>>()
-                                    .join("\n...\n")
-                                    .blue());
+                                    .join("\n")
+                            })
+                            .collect::<Vec<_>>()
+                            .join("\n...\n")
+                            .blue()
+                    );
                     if let LintMessage::SyntaxError(_) = l.msg {
-                        // print a bit before and a bit after 
+                        // print a bit before and a bit after
                         // using colors to highlight the error
                         let start = l.loc[0].start_byte();
                         let end = l.loc[0].end_byte();
-                        let before = &bib.content[(start - 10).max(0)..start];
-                        let after = &bib.content[end..(end + 10).min(bib.content.len())];
+                        let (before, error, after) = windowed(&bibtex.content, start, end, 20);
+
                         print!("{}", before);
-                        print!("{}", &bib.content[start..end].red());
+                        print!("{}", error.red());
                         print!("{}", after);
                     }
                     println!();
@@ -264,10 +321,21 @@ fn main() {
             let inputs = cargs.files.list_files();
 
             let mut format_options = FormatOptions::new(&mut db);
-            format_options.sort_entries = true;
+            if !cargs.remove_field.is_empty() {
+                format_options.blacklist = Some(cargs.remove_field);
+            }
+            if !cargs.keep_field.is_empty() {
+                format_options.whitelist = Some(cargs.keep_field);
+            }
+            if !cargs.entry_field.is_empty() {
+                format_options.field_filter = Some(cargs.entry_field);
+            }
+
+            format_options.sort_fields = cargs.sort_fields;
+            format_options.sort_entries = cargs.sort_entries;
 
             let mut stdout = std::io::stdout();
-            
+
             for bib in inputs {
                 let bibtex = BibFile::new(&bib.content);
                 let max_field_length = bibtex
@@ -279,7 +347,9 @@ fn main() {
                             .map(|field| bibtex.get_slice(field.name).len())
                             .max()
                             .unwrap_or(0)
-                    }).max().unwrap_or(0);
+                    })
+                    .max()
+                    .unwrap_or(0);
                 format_options.min_field_length = Some(max_field_length);
                 if cargs.to_file {
                     let newpath = bib.name.with_extension("new.bib");
@@ -291,16 +361,15 @@ fn main() {
                 } else {
                     write_bibfile(&bibtex, &format_options, &mut stdout);
                 }
-
             }
         }
         SubCommand::Setup(cargs) => {
             use bibadac::setup::{DownloadHandler, DownloadRequest};
             let files = cargs.files.list_files();
             let downloader = bibadac::setup::DxDoiDownloader::default();
-            let mut dois    : HashSet<String> = HashSet::new();
-            let mut eprints : HashSet<String> = HashSet::new();
-            let mut sha256s : HashSet<String> = HashSet::new();
+            let mut dois: HashSet<String> = HashSet::new();
+            let mut eprints: HashSet<String> = HashSet::new();
+            let mut sha256s: HashSet<String> = HashSet::new();
             for bib in files {
                 let bibtex = BibFile::new(&bib.content);
                 for entry in bibtex.list_entries() {
@@ -324,7 +393,7 @@ fn main() {
                 }
             }
 
-            // if there is an "output bibfile", we first open this 
+            // if there is an "output bibfile", we first open this
             // file to check if some dois already have been downloaded
             // and we remove them from the list of dois to download
             if let Some(path) = &cargs.to_file {
@@ -345,23 +414,24 @@ fn main() {
             }
 
             let rt = tokio::runtime::Builder::new_current_thread()
-                        .enable_io()
-                        .enable_time()
-                        .build()
-                        .unwrap();
+                .enable_io()
+                .enable_time()
+                .build()
+                .unwrap();
 
-            let doi_requests : Vec<_> = 
-                dois.iter().map(|d| DownloadRequest::Doi(d)).collect();
+            let doi_requests: Vec<_> = dois.iter().map(|d| DownloadRequest::Doi(d)).collect();
 
             if cargs.progress {
                 println!("{} {} dois", "[TOTAL]".blue(), dois.len());
             }
-            rt.block_on(async { 
-                let res = downloader.download(&doi_requests, |url| {
-                    if cargs.progress {
-                        println!("{} {}", "[Downloading]".green(), url);
-                    }
-                }).await;
+            rt.block_on(async {
+                let res = downloader
+                    .download(&doi_requests, |url| {
+                        if cargs.progress {
+                            println!("{} {}", "[Downloading]".green(), url);
+                        }
+                    })
+                    .await;
                 let mut count = 0;
                 for r in res.iter() {
                     if let Some(s) = r {
@@ -376,7 +446,7 @@ fn main() {
                 }
                 if let Some(path) = cargs.to_file {
                     use std::io::Write;
-                    // we append to the file and create the file if it 
+                    // we append to the file and create the file if it
                     // does not exist
                     let mut out = std::fs::OpenOptions::new()
                         .create(true)
